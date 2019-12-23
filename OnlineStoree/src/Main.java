8287aae09875ed7b74ff48e8b5b76ab9396fd5a3
MahmoudMAmr <57380255+MahmoudMAmr@users.noreply.github.com>
2019-12-24 01:48:42 +0200

    Update Main.java

--- OnlineStoree/src/Main.java
+++ OnlineStoree/src/Main.java
@@ -4,10 +4,6 @@ import java.util.Scanner;
 
 public class Main {
 public static void main(String[] args) throws IOException {
-	Administrator x= new Administrator();
-	x.addNewProduct();
-       
-        
        Account y= new Account();
 	y.Account();
        Customer c = new Customer();
